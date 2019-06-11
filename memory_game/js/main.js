79ca430c937259fa162c35f9b30306dcafaf8b48
Your Name <you@example.com>
2019-06-11 09:56:51 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,28 @@
-var cards = ["queen", "queen", "king", "king"];
+// JS file of memory game
+
+var cards = [
+{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch (){
@@ -10,9 +34,10 @@ function checkForMatch (){
 };
 
 function flipCard (cardID) {
-    console.log("User flipped " + cards[cardID]);
-
-    cardsInPlay.push(cards[cardID]);
+    console.log("User flipped " + cards[cardID].rank);
+    console.log(cards[cardID].cardImage);
+    console.log(cards[cardID].suit);
+    cardsInPlay.push(cards[cardID].rank);
 
     if (cardsInPlay.length === 2){
         checkForMatch();
